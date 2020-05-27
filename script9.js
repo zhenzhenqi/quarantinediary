232a5f630c9e1fa91ae265a2bcce2c33986192e5
zhenzhenqi <zhenzhenqi@gmail.com>
2020-05-27 02:12:15 -0400

    metaball bg to grey

--- script9.js
+++ script9.js
@@ -1,36 +1,31 @@
 var blobs = []
 
 function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.parent('myContainer');
-	colorMode(HSB);
-  for (i = 0; i < 18; i++) blobs.push(new Blob(width/2,height/2));
+  createCanvas(400, 400);
+  // colorMode(HSB);
+  for (i = 0; i < 10; i++) blobs.push(new Blob(random(0, width), random(0, height)));
 }
 
 function draw() {
   background(0);
+
   loadPixels();
   for (x = 0; x < width; x++) {
     for (y = 0; y < height; y++) {
       let sum = 0;
       for (i = 0; i < blobs.length; i++) {
-				//let d = dist(x,y,blobs[i].x,blobs[i].y);//too heavy...
-      	let xdif = x - blobs[i].x;
-				let ydif = y - blobs[i].y;
-      	let d = sqrt((xdif * xdif) + (ydif * ydif));
-        sum += 10 * blobs[i].r*10 / d;
+        let xdif = x - blobs[i].x;
+        let ydif = y - blobs[i].y;
+        let d = sqrt((xdif * xdif) + (ydif * ydif));
+        sum += 10 * blobs[i].r / d;
       }
-			if(sum > 230  && sum <255){
-				set(x, y, color(map(sum,230,255,0,255), 0, 255));
-			}
-
+      set(x, y, color(sum, sum, sum));
     }
   }
   updatePixels();
 
   for (i = 0; i < blobs.length; i++) {
     blobs[i].update();
-		//blobs[i].show();
   }
 }
 
@@ -42,19 +37,20 @@ class Blob {
     let angle = random(0, 2 * PI);
     this.xspeed = random(2, 5) * Math.cos(angle);
     this.yspeed = random(2, 5) * Math.sin(angle);
-    this.r = random(12, 24);
+    this.r = random(120, 240);
   }
 
   update() {
     this.x += this.xspeed;
     this.y += this.yspeed;
-    if (this.x > width*0.8 || this.x < width*0.2) this.xspeed *= -1;
-    if (this.y > height*0.8 || this.y <  height*0.2) this.yspeed *= -1;
+    if (this.x > width || this.x < 0) this.xspeed *= -1;
+    if (this.y > height || this.y < 0) this.yspeed *= -1;
   }
 
   show() {
-		fill(255,0,255);
-		noStroke();
+    noFill();
+    stroke(0);
+    strokeWeight(4);
     ellipse(this.x, this.y, this.r * 2, this.r * 2);
   }
 }
