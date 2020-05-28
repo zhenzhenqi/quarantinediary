5e396844acd3c3458af0502bd6e66a613395973f
zhenzhenqi <zhenzhenqi@gmail.com>
2020-05-28 17:28:08 -0400

    day7

--- script7.js
+++ script7.js
@@ -1,36 +1,66 @@
-var bg, img;
+var elem = document.getElementById("txt");
+var elemX = window.height/2;
+
+window.onload = function() {
+  setInterval(frame, 500);
+};
+
+function frame() {
+
+      // elem.style.top = pos + "px";
+
+      elem.style.top = window.Width/2 + "px";
+}
+
+var img;
 var xpos;
 var ypos;
 var xdir;
 var ydir;
 var speed;
 var scale;
+var txt;
 
 function preload() {
-  // bg = color(255, 0 , 0);
-  bg = loadImage("/images/nebula.jpg");
   img = loadImage("/images/check.jpg");
-  // console.log(img);
 }
 
 function setup() {
   colorMode(HSB, 100, 255);
+  rectMode(CENTER);
+
+  stroke(255, 0, 255);
+
   let cnv = createCanvas(windowWidth, windowHeight);
   cnv.parent('myContainer');
-  // background(bg);
-  background(0, 0, 0);
+  background(0);
+
+  txt = new Txt();
   xdir = 1;
   ydir = 1;
   scale = 0.5;
   speed = 40;
   xpos = 28;
   ypos = 0;
+
 }
 
 function draw() {
-  tint(random(0, 255),random(0, 255), random(0, 255), 126);
+  elemX = elemX-5;
+  elem.style.left = elemX + "px";
+
+  textSize(40);
+  fill(random(0, 100), 255, 120);
+  stroke(255, 0, 255);
+  strokeWeight(4);
+
+  // txt.update();
+  // txt.display();
+  // console.log(txt);
+  tint(random(0, 255), random(0, 255), random(50, 200), 50);
   image(img, xpos, ypos, img.width * scale, img.height * scale);
   if (frameCount % 19 == 0) {
+    // text("how much does it cost to fix everything?", random(0, windowWidth), random(0, windowHeight));
     xpos += xdir * speed;
     ypos += ydir * speed;
     speed += 1;
@@ -45,26 +75,42 @@ function draw() {
   }
 }
 
-class txt {
-  constructor(_index) {
-    this.xloc = windowWidth+100;
-    this.yloc = random(100, windowHeight-100);
-    this.index = _index;
+class Txt {
+  constructor() {
+    this.xloc = windowWidth/2;
+    this.yloc = windowHeight/2;
+    this.msg = "this is a test";
     // let angle = random(0, 2 * PI);
-    this.xspeed = random(-1, -3);
+    // this.xspeed = -1*random(40, 60);
+    this.xspeed = -5;
     // this.yspeed = random(1, 3);
   }
 
   update() {
-    this.x += this.xspeed;
+    this.xloc += this.xspeed;
     // this.y += this.yspeed;
-    if (this.x <-100 ) this.xloc = -100;
+    if (this.xloc <-100 ) this.xloc = windowWidth + 100;
   }
 
-  show() {
-    noFill();
-    stroke(255, 0, 0);
-    strokeWeight(4);
-    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  display() {
+    text(this.msg, this.xloc, this.yloc);
   }
 }
+
+// class Jitter {
+//   constructor() {
+//     this.x = random(width);
+//     this.y = random(height);
+//     this.diameter = random(10, 30);
+//     this.speed = 1;
+//   }
+//
+//   move() {
+//     this.x += random(-this.speed, this.speed);
+//     this.y += random(-this.speed, this.speed);
+//   }
+//
+//   display() {
+//     ellipse(this.x, this.y, this.diameter, this.diameter);
+//   }
+// }
