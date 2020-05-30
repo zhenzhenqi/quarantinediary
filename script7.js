002f610bd38968eee43f6ffd7f63d03e10addcf1
zhenzhenqi <zhenzhenqi@gmail.com>
2020-05-30 15:16:23 -0400

    day7

--- script7.js
+++ script7.js
@@ -6,9 +6,9 @@ import * as THREE from './three/three.module.js';
 import {
   Water
 } from './three/Water.js';
-// import {
-//   Sky
-// } from './three/Sky.js';
+import {
+  Sky
+} from './three/Sky.js';
 
 import {
   FresnelShader
@@ -19,8 +19,9 @@ var container, stats;
 var camera, scene, renderer, light;
 var controls, water, avatar;
 var spheres = [];
-var mouseX = 0,
-  mouseY = 0;
+var mouseX = 0, mouseY = 0;
+// var fogColor;
+
 document.addEventListener('mousemove', onDocumentMouseMove, false);
 
 var windowHalfX = window.innerWidth / 2;
@@ -31,7 +32,7 @@ animate();
 
 function init() {
   //bg cube
-  var path = "./three/textures/skybox_hell/";
+  var path = "./three/textures/skybox_pixel/";
   var format = '.png';
   var urls = [
     path + 'px' + format, path + 'nx' + format,
@@ -43,6 +44,8 @@ function init() {
 
   scene = new THREE.Scene();
   scene.background = textureCube;
+  // fogColor = new THREE.Color(0xffffff);
+  // scene.fog = new THREE.Fog(fogColor, 0.0025, 20);
 
   // create spheres
   var geometry = new THREE.SphereBufferGeometry(100, 32, 16);
@@ -73,19 +76,11 @@ function init() {
   container.appendChild(renderer.domElement);
 
 
-  // scene = new THREE.Scene();
-
-
-
-  // camera.position.set(30, 30, 100);
-
   camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 100000);
   camera.position.z = 3200;
 
-  light = new THREE.DirectionalLight(0x94000a, 0.8);
+  light = new THREE.DirectionalLight(0x94000a, 1);
   scene.add(light);
-
-
   var waterGeometry = new THREE.PlaneBufferGeometry(10000, 10000);
 
   water = new Water(
@@ -112,56 +107,48 @@ function init() {
 
   // Skybox
 
-  // var sky = new Sky();
-  //
-  // var uniforms = sky.material.uniforms;
-  //
-  // uniforms['turbidity'].value = 30;
-  // uniforms['rayleigh'].value = 3;
-  // uniforms['luminance'].value = 1;
-  // uniforms['mieCoefficient'].value = 0.003;
-  // uniforms['mieDirectionalG'].value = 0.9;
-  //
-  // var parameters = {
-  //   distance: 400,
-  //   inclination: 0.51,
-  //   azimuth: 0.205
-  // };
-  //
-  // var cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512, {
-  //   format: THREE.RGBFormat,
-  //   generateMipmaps: true,
-  //   minFilter: THREE.LinearMipmapLinearFilter
-  // });
-  // var cubeCamera = new THREE.CubeCamera(0.1, 1, cubeRenderTarget);
-  //
-  // scene.background = cubeRenderTarget;
-  //
-  // function updateSun() {
-  //
-  //   var theta = Math.PI * (parameters.inclination - 0.5);
-  //   var phi = 2 * Math.PI * (parameters.azimuth - 0.5);
-  //
-  //   light.position.x = parameters.distance * Math.cos(phi);
-  //   light.position.y = parameters.distance * Math.sin(phi) * Math.sin(theta);
-  //   light.position.z = parameters.distance * Math.sin(phi) * Math.cos(theta);
-  //
-  //   sky.material.uniforms['sunPosition'].value = light.position.copy(light.position);
-  //   water.material.uniforms['sunDirection'].value.copy(light.position).normalize();
-  //
-  //   cubeCamera.update(renderer, sky);
-  // }
-  //
-  // updateSun();
+  var sky = new Sky();
 
+  var uniforms = sky.material.uniforms;
 
+  uniforms['turbidity'].value = 30;
+  uniforms['rayleigh'].value = 3;
+  uniforms['luminance'].value = 1;
+  uniforms['mieCoefficient'].value = 0.001;
+  uniforms['mieDirectionalG'].value = 0.8;
 
-  // controls = new OrbitControls(camera, renderer.domElement);
-  // controls.maxPolarAngle = Math.PI * 0.495;
-  // controls.target.set(0, 10, 0);
-  // controls.minDistance = 40.0;
-  // controls.maxDistance = 200.0;
-  // controls.update();
+  var parameters = {
+    distance: 100,
+    inclination: 0.51,
+    azimuth: 0.2
+  };
+
+  var cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512, {
+    format: THREE.RGBFormat,
+    generateMipmaps: true,
+    minFilter: THREE.LinearMipmapLinearFilter
+  });
+  var cubeCamera = new THREE.CubeCamera(0.1, 1, cubeRenderTarget);
+
+  scene.background = cubeRenderTarget;
+
+  function updateSun() {
+
+    var theta = Math.PI * (parameters.inclination - 0.5);
+    var phi = 2 * Math.PI * (parameters.azimuth - 0.5);
+
+    light.position.x = parameters.distance * Math.cos(phi);
+    light.position.y = parameters.distance * Math.sin(phi) * Math.sin(theta);
+    light.position.z = parameters.distance * Math.sin(phi) * Math.cos(theta);
+
+    sky.material.uniforms['sunPosition'].value = light.position.copy(light.position);
+    water.material.uniforms['sunDirection'].value.copy(light.position).normalize();
+
+    cubeCamera.update(renderer, sky);
+  }
+
+  updateSun();
+
 
   window.addEventListener('resize', onWindowResize, false);
 
@@ -169,7 +156,7 @@ function init() {
 
 function onWindowResize() {
 
-  windowHalfX = window.innerWidth / 2;
+  windowHalfX = window.innerWidth/2;
   windowHalfY = window.innerHeight / 2;
 
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -192,9 +179,9 @@ function animate() {
 }
 
 function render() {
-  camera.position.x += (mouseX - camera.position.x) * .05;
-  camera.position.y += (-mouseY - camera.position.y) * .05;
-  camera.position.y = Math.max(camera.position.y, 10);
+  camera.position.x += (-mouseX*7 - camera.position.x)* 0.5;
+  camera.position.y += (-mouseY - camera.position.y) * .03;
+  camera.position.y = Math.max(camera.position.y, 1);
 
   // console.log(camera.position.y);
   camera.lookAt(scene.position);
@@ -205,11 +192,12 @@ function render() {
   for (var i = 0, il = spheres.length; i < il; i++) {
     var sphere = spheres[i];
     sphere.position.y = Math.sin(time + i*3) * 20 + i*3;
+    // sphere.rotation.x = Math.cos(time + i*3);
     // sphere.position.x = Math.cos(time + i);
 
     // sphere.position.x = 100 * Math.cos( time + i );
     // sphere.position.y = 100 * Math.sin( time + i * 1.1 );
   }
-  water.material.uniforms['time'].value += 1.0 / 60.0;
+  water.material.uniforms['time'].value += 1.0 / 110.0;
   renderer.render(scene, camera);
 }
