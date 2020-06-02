51e52d05057ba0c207f6505c0df7b5eb6b0253c1
zhenzhenqi <zhenzhenqi@gmail.com>
2020-06-02 17:23:40 -0400

    model

--- diary.js
+++ diary.js
@@ -11,7 +11,7 @@ let currentLen = 0;
 function setup() {
   noCanvas();
   // Create the LSTM Generator passing it the model directory
-  charRNN = ml5.charRNN('./models/input2', modelReady);
+  charRNN = ml5.charRNN('./models/input', modelReady);
 
   // Grab the input text
   textInput = select('#textInput');
